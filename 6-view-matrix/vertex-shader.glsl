e5af0e11507e2cea16b2d1c5f0f198c572f742c3
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-05-26 09:00:23 +0200

    Started 6-model-matrix.

---
+++ 6-view-matrix/vertex-shader.glsl
@@ -0,0 +1,23 @@
+#version 330 core
+
+in vec3 position;
+in vec4 vColor;
+
+out vec4 fColor; // This name must match the name in the fragment shader in order to work
+
+uniform mat4 transform_x; // the transformation matrix for x
+uniform mat4 transform_y; // the transformation matrix for y
+uniform mat4 transform_z; // the transformation matrix for z
+
+uniform mat4 transform_translation;
+uniform mat4 transform_scaling;
+
+in vec2 vTexcoord;   // passthrough shading for interpolated textures
+out vec2 fTexcoord;
+
+void main() {
+  mat4 model = transform_translation * transform_scaling * transform_z * transform_y * transform_x;
+  gl_Position = model * vec4(position, 1.0f);
+  fColor = vColor;
+  fTexcoord = vTexcoord;
+}
