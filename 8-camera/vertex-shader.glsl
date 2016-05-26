5b4d62e68a8efc831d915f795d39c80e976e5537
Alexander Lingtorp <alexander.lingtorp@gmail.com>
2016-05-26 20:16:00 +0200

    Started working on chapter #8: Camera.

---
+++ 8-camera/vertex-shader.glsl
@@ -0,0 +1,35 @@
+#version 330 core
+
+in vec3 position;
+in vec4 vColor;
+
+out vec4 fColor; // This name must match the name in the fragment shader in order to work
+
+// Model
+uniform mat4 transform_x; // the transformation matrix for x
+uniform mat4 transform_y; // the transformation matrix for y
+uniform mat4 transform_z; // the transformation matrix for z
+
+uniform mat4 transform_translation;
+uniform mat4 transform_scaling;
+
+// View
+uniform mat4 transform_cam_x; // the transformation matrix for x
+uniform mat4 transform_cam_y; // the transformation matrix for y
+uniform mat4 transform_cam_z; // the transformation matrix for z
+uniform mat4 transform_cam_translation;
+
+// Projection
+uniform mat4 transform_perspective;
+
+in vec2 vTexcoord;   // passthrough shading for interpolated textures
+out vec2 fTexcoord;
+
+void main() {
+  mat4 model = transform_translation * transform_scaling * transform_z * transform_y * transform_x;
+  mat4 view  = transform_cam_translation * transform_cam_x * transform_cam_y * transform_cam_z;
+  mat4 projection = transform_perspective;
+  gl_Position = projection * view * model * vec4(position, 1.0f);
+  fColor = vColor;
+  fTexcoord = vTexcoord;
+}
